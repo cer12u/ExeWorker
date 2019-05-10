95cdc759f648918aeb2ce90e1fd96636fb15bfe3
Yasu <ya712og95d@gmail.com>
2019-05-11 07:33:40 +0900

    定期commit

---
+++ Browser/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Browser
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Net.HttpWebRequest wr =(System.Net.HttpWebRequest) System.Net.WebRequest.Create("http://yahoo.co.jp");
+            wr.Method = "GET";
+
+
+
+            System.Net.WebResponse res = wr.GetResponse();
+            System.IO.Stream rs = res.GetResponseStream();
+
+            wb.NavigateToStream(rs);
+
+            //wb.Source = rs;
+            //wb.Navigate("https://www.msn.com/ja-jp");
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NestedClass ns = new NestedClass();
+
+            ns.SetValue(ns, "Param[1][1][2]", 1);
+
+            ns.SetValue(ns, "Param", 1);
+
+        }
+
+        public class NestedClass
+        {
+            public int Param { get; set; } = 0;
+            public List<int> ParamL1 { get; set; } = new List<int>();
+            public List<List<int>> ParamL2 { get; set; } = new List<List<int>>();
+            public List<List<List<int>>> ParamL3 { get; set; } = new List<List<List<int>>>();
+            public List<List<List<List<int>>>> ParamL4 { get; set; } = new List<List<List<List<int>>>>();
+
+            public bool SetValue(object obj, string name, int value)
+            {
+                //System.Reflection.PropertyInfo ip = obj.GetType().GetProperty("ParamL2");
+                //if (ip is null)
+                //    return false;
+                if (typeof(System.Collections.IEnumerable).IsAssignableFrom(obj.GetType())){
+                    Console.WriteLine("Assignable");
+                }
+
+                Type tp = obj.GetType();
+                foreach(System.Reflection.PropertyInfo pi in tp.GetType().GetProperties())
+                {
+                    Type ttp = pi.PropertyType;
+                    if(ttp == typeof(System.String))
+                    {
+                        Console.WriteLine("String");
+                    }
+                    else if (typeof(System.Collections.IEnumerable).IsAssignableFrom(ttp))
+                    {
+                        Console.WriteLine(pi.Name);
+                        Console.WriteLine("Match");
+                    }
+                }
+
+                //Console.WriteLine("FieldInfo");
+                //foreach (System.Reflection.FieldInfo fi in tp.GetType().GetFields())
+                //    Console.WriteLine(fi.Name);
+                //Console.WriteLine("MemberInfo");
+                //foreach (System.Reflection.MemberInfo mi in tp.GetType().GetMethods())
+                //    Console.WriteLine(mi.Name);
+                
+                return false;
+            }
+
+        }
+    }
+}

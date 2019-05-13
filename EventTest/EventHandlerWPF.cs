d8ba9b55d22dddda07ee39d0a428dd601b35d156
Yasu <ya712og95d@gmail.com>
2019-05-14 07:53:44 +0900

    EventHandlerテスト

---
+++ EventTest/EventHandlerWPF.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace EventTest
+{
+    class EventHandlerWPF
+    {
+
+
+        public class TimerClass: IDisposable
+        {
+
+            public delegate void EventDelegate();
+            public event EventDelegate EventHdr;
+
+
+            System.Timers.Timer t = new System.Timers.Timer();
+
+
+            public void StartTimer(uint ms)
+            {
+
+                t.Interval = ms;
+                t.Elapsed += TimerEvent;
+                t.Start();
+
+            }
+
+            private void TimerEvent(object sender, ElapsedEventArgs e)
+            {
+                if (EventHdr != null)
+                    EventHdr();
+            }
+
+            public void StopTimer()
+            {
+                if(t.Enabled)
+                    t.Stop();
+            }
+
+            public void Dispose()
+            {
+                StopTimer();
+            }
+
+
+        }
+
+
+    }
+
+
+}
